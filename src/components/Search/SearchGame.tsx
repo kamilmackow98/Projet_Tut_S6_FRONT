dc5f75860b98f29df629784fac5ef190c9cce3a7
Snorloux <margot.dumoulin18@gmail.com>
2021-03-30 22:17:30 +0200

    :sparkles: Paginated game name autocomplete

--- src/components/Search/SearchGame.tsx
+++ src/components/Search/SearchGame.tsx
@@ -1,11 +1,13 @@
 import { TextField } from "@material-ui/core";
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Game } from "types";
 
 const SearchGame = () => {
 
 	const [gameNames, setGameNames] = useState<Game[]>([]);
+	const [inputGameNameSearch, setInputGameNameSearch] = useState("");
+	const [gameNamePagination, setGameNamePagination] = useState(0);
 
 	useEffect(() => {
 		fetch(`/api/games`)
@@ -17,13 +19,49 @@ const SearchGame = () => {
 		.catch((e) => console.error(e));
 	}, []);
 
+	useEffect(() => {
+		setGameNamePagination(0);
+		setGameNames([]);
+		fetch(`/api/games?name=${inputGameNameSearch}`)
+		.then((res) => res.json())
+		.then((games: Game[]) => {
+			console.log(games);
+			setGameNames(games);
+		})
+		.catch((e) => console.error(e));
+	}, [inputGameNameSearch]);
+
+	useEffect(() => {
+		fetch(`/api/games?name=${inputGameNameSearch}&page=${gameNamePagination}`)
+		.then((res) => res.json())
+		.then((games: Game[]) => {
+			const extendedGames: Game[] = gameNames.concat(games);
+			setGameNames(extendedGames);
+		})
+		.catch((e) => console.error(e));
+	}, [gameNamePagination])
+	
+	const handleChange = (value: string) => {
+		setInputGameNameSearch(value)
+	}
+
 	return (
 		<Autocomplete
-			id="combo-box-demo"
+			ListboxProps={{
+				style: { maxHeight: 200, overflow: 'auto' },
+				onScroll: (event: React.SyntheticEvent) => {
+					const listboxNode = event.currentTarget;
+					if (listboxNode.scrollTop + listboxNode.clientHeight === listboxNode.scrollHeight) {
+						setGameNamePagination(gameNamePagination + 1);
+					}
+				}
+			}}
+			freeSolo
+			id="combo-box-game-name"
 			options={gameNames}
 			getOptionLabel={(option: Game) => option.name}
 			style={{ width: 300 }}
-			renderInput={(params) => <TextField {...params} label="Game's name" variant="outlined" />}
+			renderInput={(params) => <TextField {...params} label="Game's name" variant="outlined" onChange={event => handleChange(event.target.value)}/>}
 		/>
 	);
 };
