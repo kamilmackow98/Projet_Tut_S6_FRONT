4e40ea01ecb52f2c7c91c19b7482cf3f22d96e80
Kamil Mackow <kamilmackow98@gmail.com>
2021-04-01 11:16:15 +0200

    :construction: Display recommended games

--- src/components/Game/Recommended.tsx
+++ src/components/Game/Recommended.tsx
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Chip from "@material-ui/core/Chip";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 import { sortByOccurrences } from "utils/ObjectUtils";
-import { Container, Grid } from "@material-ui/core";
-import { countOccurrences, getRandomFromArray } from "utils/ArrayUtils";
+import { countOccurrences } from "utils/ArrayUtils";
 import { CompleteGameInfo } from "types";
 import Loader from "components/Layout/Loader";
 import GameCard from "./GameCard";
+import React, { useState, useEffect } from "react";
 
 const TOP_TAGS_LENGTH = 5;
 let tags: string[] = [];
 
-function cleanArrays() {
+function clearArrays() {
 	tags = [];
 }
 
-const Recommended = () => {
-	// Here we're gonna get user's favorites games by id, and then search 10 related / recommended games
+const Recommended: React.FC = () => {
+	// Here we're gettting user's favorites games for now (mock the data)
 	const [favorites] = useState<number[]>([
 		501500,
 		822110,
@@ -42,7 +46,11 @@ const Recommended = () => {
 		807000,
 	]);
 	const [favoritesData, setFavoritesData] = useState<CompleteGameInfo[]>([]);
+	const [recommendedGames, setRecommendedGames] = useState<CompleteGameInfo[]>(
+		[]
+	);
 	const [loading, setLoading] = useState(true);
+	const [topTags, setTags] = useState<string[]>([]);
 
 	useEffect(() => {
 		if (favorites.length > 0) {
@@ -54,7 +62,8 @@ const Recommended = () => {
 				)
 			)
 				.then((data) => {
-					data = data.filter((item) => item); // Get rid of null / undefined items
+					// Get rid of null / undefined items
+					data = data.filter((item) => item);
 					setFavoritesData(data);
 				})
 				.catch((e) => console.error(e));
@@ -67,37 +76,87 @@ const Recommended = () => {
 				tags = tags.concat(entry.steamspy_tags.map((tag) => tag));
 			});
 
-			// TODO : Here look for 10+ games based on random tags and set recommended
-			const topTags = sortByOccurrences(countOccurrences(tags)).slice(0, TOP_TAGS_LENGTH);
-			const randomTag = getRandomFromArray(topTags);
-			console.log(randomTag);
+			const sortedTags = sortByOccurrences(countOccurrences(tags)).slice(
+				0,
+				TOP_TAGS_LENGTH
+			);
 
-			setTimeout(() => {
-				setLoading(false);
-			}, 1000);
+			setTags(sortedTags);
+
+			fetch("/api/games", {
+				method: "POST",
+				body: JSON.stringify({ steamspy_tags: sortedTags }),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					setRecommendedGames(data);
+					setTimeout(() => {
+						setLoading(false);
+					}, 250);
+				})
+				.catch((e) => console.error(e));
 		}
 
 		return () => {
-			cleanArrays();
+			clearArrays();
 		};
 	}, [favoritesData]);
 
 	useEffect(() => {
 		return () => {
-			cleanArrays();
+			clearArrays();
 		};
 	}, []);
+
+	const gamesMap = recommendedGames.map((card) => (
+		<Grid item key={card.id} xs={12} sm={10} md={6} lg={4}>
+			<GameCard {...card} />
+		</Grid>
+	));
+
+	const tagsMap = React.Children.toArray(
+		topTags.map((tag) => (
+			<Grid item>
+				<Chip color="primary" label={tag} />
+			</Grid>
+		))
+	);
 
 	if (loading) return <Loader />;
 
 	return (
 		<Container fixed disableGutters>
+			<Grid container justify={"center"}>
+				<Grid item xs={12} sm={10} md={12}>
+					<Typography paragraph variant="h6">
+						RECOMMENDED FOR YOU
+					</Typography>
+					<Box mb={2}>
+						<Grid
+							justify="flex-start"
+							alignItems="center"
+							spacing={1}
+							container
+						>
+							<Grid item>
+								<Typography variant="body2">
+									Because you like games with tags
+								</Typography>
+							</Grid>
+							<Grid item>
+								<Grid container spacing={1}>
+									{tagsMap}
+								</Grid>
+							</Grid>
+						</Grid>
+					</Box>
+				</Grid>
+			</Grid>
 			<Grid container spacing={3} justify={"center"}>
-				{favoritesData.map((card) => (
-					<Grid item key={card.id} xs={12} sm={10} md={6} lg={4}>
-						<GameCard {...card} />
-					</Grid>
-				))}
+				{gamesMap}
 			</Grid>
 		</Container>
 	);
