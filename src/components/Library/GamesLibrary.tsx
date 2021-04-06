1bfd683a9ab5fd87a513632108f4f603a0a6b81b
Snorloux <margot.dumoulin18@gmail.com>
2021-04-06 15:24:17 +0200

    :construction: add message when no games found

--- src/components/Library/GamesLibrary.tsx
+++ src/components/Library/GamesLibrary.tsx
@@ -102,6 +102,7 @@ const GamesLibrary = () => {
                 : 
                 <Grid item xs={12}>
                     <NoGamesFound />
+                    <h3 className={classes.noGamesFoundContainer}>We could show you suggestions based on your library !</h3>
                 </Grid>
             }
             <Grid item xs={12} sm={12} className={classes.paginationContainer}>
