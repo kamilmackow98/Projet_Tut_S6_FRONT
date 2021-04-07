be08598ad073bd3f2f65b8204844b4841b05d1da
Snorloux <margot.dumoulin18@gmail.com>
2021-04-07 22:43:48 +0200

    :recycle: Update error management for all autocompletes

--- src/components/Search/Autocomplete/AutocompleteGameName.tsx
+++ src/components/Search/Autocomplete/AutocompleteGameName.tsx
@@ -1,13 +1,13 @@
 import { TextField } from "@material-ui/core";
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import React, { useEffect, useState } from "react";
-import { Game, GameSearchResult } from "types";
+import { APIErrorMessage, Game, GameSearchResult } from "types";
 import { debounce } from "lodash";
 
 interface Props {
 	onChangeName: Function,
 	mustClear: boolean
-}
+};
 
 const AutocompleteGameName: React.FC<Props> = ({ onChangeName, mustClear }) => {
 
@@ -22,17 +22,6 @@ const AutocompleteGameName: React.FC<Props> = ({ onChangeName, mustClear }) => {
 	const inputGameNameSearchRef = React.useRef(inputGameNameSearch);
 	const firstLaunchRef = React.useRef(firstLaunch);
 	const keyRef = React.useRef(key);
-
-	useEffect(() => {
-		fetch(`/api/games`)
-		.then((res) => res.json())
-		.then((resJson: GameSearchResult) => {
-			setGameNames(resJson.games);
-		})
-		.catch((e) => console.error(e));
-
-		setFirstLaunch(false);
-	}, []);
 
 	useEffect(() => {
 		if (mustClear) {
@@ -51,26 +40,49 @@ const AutocompleteGameName: React.FC<Props> = ({ onChangeName, mustClear }) => {
 	});
 
 	useEffect(() => {
+		fetch(`/api/games`)
+		.then(r =>  r.json().then(data => ({status: r.status, body: data})))
+		.then((obj) => {
+			if (obj.status === 200) {
+				setGameNames((obj.body as GameSearchResult).games);
+			} else {
+				throw new Error((obj.body as APIErrorMessage).message);
+			}
+		})
+		.catch((e) => {});
+
+		setFirstLaunch(false);
+	}, []);
+
+	useEffect(() => {
 		if (!firstLaunchRef.current) {
 			setGameNamePagination(1);
 			setGameNames([]);
 			fetch(`/api/games?name=${inputGameNameSearch}`)
-			.then((res) => res.json())
-			.then((resJson: GameSearchResult) => {
-				resJson.games.forEach((game: Game) => { game.release_date = new Date(game.release_date)})
-				setGameNames(resJson.games);
+			.then(r =>  r.json().then(data => ({status: r.status, body: data})))
+			.then((obj) => {
+				if (obj.status === 200) {
+					(obj.body as GameSearchResult).games.forEach((game: Game) => { game.release_date = new Date(game.release_date)})
+					setGameNames((obj.body as GameSearchResult).games);
+				} else {
+					throw new Error((obj.body as APIErrorMessage).message);
+				}
 			})
-			.catch((e) => console.error(e));
+			.catch((e) => {});
 		}
 	}, [inputGameNameSearch]);
 
 	useEffect(() => {
 		if (!firstLaunchRef.current && gameNamePagination !== 1) {
 			fetch(`/api/games?name=${inputGameNameSearchRef.current}&page=${gameNamePagination}`)
-			.then((res) => res.json())
-			.then((resJson: GameSearchResult) => {
-				const extendedGames: Game[] = gameNamesRef.current.concat(resJson.games);
-				setGameNames(extendedGames);
+			.then(r =>  r.json().then(data => ({status: r.status, body: data})))
+			.then((obj) => {
+				if (obj.status === 200) {
+					const extendedGames: Game[] = gameNamesRef.current.concat((obj.body as GameSearchResult).games);
+					setGameNames(extendedGames);
+				} else {
+					throw new Error((obj.body as APIErrorMessage).message);
+				}
 			})
 			.catch((e) => console.error(e));
 		}
