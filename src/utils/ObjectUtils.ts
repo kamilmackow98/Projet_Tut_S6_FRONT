c0f919b3cb7e301d2eba4f0602424456a1ec4d17
Kamil Mackow <kamilmackow98@gmail.com>
2021-04-07 15:54:08 +0200

    :fire: Remove unused files

--- src/utils/ObjectUtils.ts
+++
@@ -1,9 +0,0 @@
-export function sortByOccurrences(obj: any) {
-	const keysSorted = Object.keys(obj)
-		.sort(function (a, b) {
-			return obj[a] - obj[b];
-		})
-		.reverse();
-
-	return keysSorted;
-}
